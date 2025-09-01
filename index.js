43235b4f515d3ed588528dc4aeea610e4e60c3b9
Rahat92 <khrahat92@gmail.com>
2025-09-01 17:23:45 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+
+const currentDate = new Date();
+const currentMonthNum = new Date().getMonth()
+const currentDateNum = new Date().getDate()
+const currentYear = currentDate.getFullYear();
+const yymmdd = `${currentYear}${currentMonthNum.toString().length < 2 ? '0' + (currentMonthNum + 1).toString() : (currentMonthNum + 1).toString()}${currentDateNum.toString().length < 2 ? '0' + currentDateNum.toString() : currentDateNum.toString()}`
+console.log(yymmdd)
+
+
+const dir_dse_merged_eod = `C:\\Users/ASUS/Desktop/merged_eod/merged-eod(20250720_${yymmdd}).csv`;
+const dir_amar_stock_merged_eod = `C:\\Users/ASUS/Desktop/merged_eod/merged-amar_stock_eod(20250720_${yymmdd}).csv`;
+const output_file = 'C:\\Users/ASUS/Desktop/merged_eod/final-merged-result.csv';
+const dse_dsex_file = 'C:\\Users/ASUS/Downloads/dsex.csv';
+const modified_dir = `E:\\amar_stock_modified`;
+
+
+
+const modified_files = fs.readdirSync(modified_dir);
+console.log(modified_files)
+function readCsv(file) {
+    return new Promise((resolve, reject) => {
+        const rows = [];
+        fs.createReadStream(file)
+            .pipe(csvParser())
+            .on('data', row => rows.push(row))
+            .on('end', () => resolve(rows))
+            .on('error', reject);
+    });
+}
+
+(async () => {
+    try {
+        const results = await readCsv(dir_dse_merged_eod);
+        const result_amar_stock = await readCsv(dir_amar_stock_merged_eod);
+        const result_dsex = await readCsv(dse_dsex_file);
+
+        const modified_result_of_dse = results.map(item => ({
+            ...item,
+            identifier: `${item.TradeDate}${item.Open}${item.High}${item.Low}${item.Close}`,
+            Volume: null
+        }));
+
+        const amarStockLookup = {};
+        result_amar_stock.forEach(item => {
+            const identifier = `${item.Date}${item.Open}${item.High}${item.Low}${item.Close}`;
+            amarStockLookup[identifier] = item.Volume;
+        });
+
+        const modify_dsex = result_dsex.map(item => {
+            let parts = item.Date.includes('-') ? item.Date.split('-') : item.Date.split('/');
+            if (parts[0].length === 4) {
+                return { ...item, Date: parts.join('') };
+            }
+            const date = `${parts[2]}${parts[1].padStart(2, '0')}${parts[0].padStart(2, '0')}`;
+            return { ...item, Date: date };
+        });
+
+        const dsex_lookup = {};
+        modify_dsex.forEach(item => {
+            dsex_lookup[`${item.Date}-DSEX_DATE`] = item.Date;
+            dsex_lookup[`${item.Date}-DSEX_TOTAL_TRADE`] = item['Total Trade'];
+            dsex_lookup[`${item.Date}-DSEX_TOTAL_VOLUME`] = item['Total Volume'];
+            dsex_lookup[`${item.Date}-DSEX_TOTAL_VALUE`] = item['Total Value in Taka (mn)'];
+        });
+
+        const modifiedFilesLookUp = {};
+        modified_files.forEach(f => {
+            modifiedFilesLookUp[f.split('_')[0]] = f;
+        });
+
+        const mergedResult = modified_result_of_dse
+            .filter(item => item.SecurityCode !== 'DS30' && item.SecurityCode !== 'DSES')
+            .map(item => {
+                if (amarStockLookup[item.identifier]) {
+                    return { ...item, Volume: amarStockLookup[item.identifier] };
+                }
+                if (item.TradeDate === dsex_lookup[`${item.TradeDate}-DSEX_DATE`] && item.SecurityCode === 'DSEX') {
+                    return {
+                        ...item,
+                        Volume: dsex_lookup[`${item.TradeDate}-DSEX_TOTAL_VOLUME`],
+                        ['Total Value in Taka (mn)']: dsex_lookup[`${item.TradeDate}-DSEX_TOTAL_VALUE`],
+                        ['Total Trade']: dsex_lookup[`${item.TradeDate}-DSEX_TOTAL_TRADE`],
+                    };
+                }
+                return item;
+            });
+
+        const tempObj = {};
+        for (const row of mergedResult) {
+            if (modifiedFilesLookUp[row.TradeDate]) {
+                tempObj[row.TradeDate] = await readCsv(`${modified_dir}/${modifiedFilesLookUp[row.TradeDate]}`);
+            }
+        }
+        const finalMergedData = mergedResult.map(item => {
+            if (tempObj[item.TradeDate]) {
+                const foundEle = tempObj[item.TradeDate].find(ele =>
+                    `${ele['TRADING CODE']}` === `${item.SecurityCode}`
+                );
+
+                if (foundEle) {
+                    return {
+                        ...item,
+                        ['Total Trade']: foundEle.Trade,
+                        ['Total Value in Taka (mn)']: foundEle['VALUE(mn)'],
+                        ['Volume']: foundEle['Volume']
+                    };
+                }
+            }
+            return item;
+        });
+
+        const csvWriter = createCsvWriter({
+            path: output_file,
+            header: [
+                { id: 'SecurityCode', title: 'SecurityCode' },
+                { id: 'ISIN', title: 'ISIN' },
+                { id: 'AssetClass', title: 'AssetClass' },
+                { id: 'CompulsorySpot', title: 'CompulsorySpot' },
+                { id: 'TradeDate', title: 'TradeDate' },
+                { id: 'Close', title: 'Close' },
+                { id: 'Open', title: 'Open' },
+                { id: 'High', title: 'High' },
+                { id: 'Low', title: 'Low' },
+                { id: 'Var', title: 'Var' },
+                { id: 'VarPercent', title: 'VarPercent' },
+                { id: 'Volume', title: 'Volume' },
+                { id: 'Total Value in Taka (mn)', title: 'Total Value in Taka (mn)' },
+                { id: 'Total Trade', title: 'Total Trade' }
+            ]
+        });
+
+        await csvWriter.writeRecords(finalMergedData);
+        console.log(`CSV file created successfully at: ${output_file}`);
+
+    } catch (err) {
+        console.error("Error:", err);
+    }
+})();
